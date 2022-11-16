306cbef2771d7c7ab631337cb06044a95f5d767c
Janvi Chitroda <janvichitroda0824@gmail.com>
2022-11-16 14:29:02 +0530

    read property file

--- test/read.py
+++ test/read.py
@@ -5,3 +5,28 @@ config.read('config.properties')
 print(config.sections())
 #x=config.get('details', 'foo')
 #print(x)
+
+"""
+FROM python:latest
+
+
+#Labels as key value pair
+LABEL Maintainer="roushan.me17"
+
+
+# Any working directory can be chosen as per choice like '/' or '/home' etc
+# i have chosen /usr/app/src
+WORKDIR /usr/app/src
+
+#to COPY the remote file at working directory in container
+COPY read.py ./
+# Now the structure looks like this '/usr/app/src/test.py'
+
+
+#CMD instruction should be used to run the software
+#contained by your image, along with any arguments.
+
+CMD [ "python", "./test/read.py"]
+
+
+"""
