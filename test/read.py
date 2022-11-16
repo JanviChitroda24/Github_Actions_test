50602b8b67cbe639a9de9a705722a1e19b9e8e94
Janvi Chitroda <janvichitroda0824@gmail.com>
2022-11-16 14:00:34 +0530

    read property file

--- test/read.py
+++ test/read.py
@@ -2,5 +2,6 @@ import configparser
 
 config = configparser.RawConfigParser()
 config.read('config.properties')
-x=config.get(config.sections()[0], 'foo')
+#print(config.sections())
+x=config.get('details', 'foo')
 print(x)
