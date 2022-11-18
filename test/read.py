d303a9c260010f6066c54b8a40daacdbeae8c311
Janvi Chitroda <janvichitroda0824@gmail.com>
2022-11-18 13:22:49 +0530

    read property file

--- test/read.py
+++ test/read.py
@@ -1,7 +1,17 @@
+import os
+
+with open(r'.\config.properties', "r") as file:
+    data = file.readlines()
+    for line in data:
+        word = line.split("=", 1)
+        print(word)
+
+"""
 import configparser
 
 config = configparser.RawConfigParser()
-config.read(r'.\config.properties')
+config.read('config.properties')
 print(config.sections())
 x=config.get('details', 'foo')
 print(x)
+"""
